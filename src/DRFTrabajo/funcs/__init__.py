b9b567a33f4ca7c96d109589a04bf10f873145ec
Francisca Contreras <franciscontreras@alumnos.uai.cl>
2023-12-13 18:25:02 -0300

    Entregable3_Contreras_Crespo_Moreno

---
+++ src/DRFTrabajo/funcs/__init__.py
@@ -0,0 +1,1 @@
+from .uf import get_ufs
---
+++ src/DRFTrabajo/funcs/uf.py
@@ -0,0 +1,6 @@
+from datetime import date
+
+def get_ufs(last_uf_known_date: date, last_uf_value: float, new_ipc: float) -> dict[date, float]:
+    # Tu implementación aquí.
+    # Calcula y devuelve un diccionario con fechas como claves y valores UF como valores.
+    pass
